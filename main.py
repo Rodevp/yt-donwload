313e101477a8db2d3e5a466971bd5e5955ae156c
Rodevp <rodriguezrodrigor12@gmail.com>
2022-12-03 16:27:30 -0500

    print info video,  download and save video

--- main.py
+++ main.py
@@ -2,10 +2,16 @@ from pytube import YouTube
 from sys import argv
 
 def main() :
+
     link = argv[1]
     yt = YouTube(link)
 
-    print(yt.title)
+    print("----INFO VIDEO----")
+    print(f"{yt.title} - {yt.views} - {yt.author}")
+
+    youtube_download = yt.streams.get_highest_resolution()
+
+    youtube_download.download("./videos")
 
 
 
